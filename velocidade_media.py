1ece7ae5c5793d6ff7b692bda3affd96c21bdb9d
Wellington-Miguel <wellington2010_silva@hotmail.com>
2022-04-12 22:33:37 -0300

    exercise velocidade media

---
+++ velocidade_media.py
@@ -0,0 +1,7 @@
+space, time_=input().split(" ")
+space =int(space)
+time_= int (time_)
+velocidade =    int(space/time_)  
+if(space>1 and space<500):
+    if(time_>1 and time_<100 ): 
+        print(velocidade)
