f6e494744fcdfae38c3f203ac633ba1856d2db75
Wellington-Miguel <wellington2010_silva@hotmail.com>
2022-04-14 20:49:45 -0300

    update first exercise:  aceleration force

--- velocidade_media.py
+++ velocidade_media.py
@@ -1,7 +1,5 @@
-space, time_=input().split(" ")
-space =int(space)
-time_= int (time_)
-velocidade =    int(space/time_)  
-if(space>1 and space<500):
-    if(time_>1 and time_<100 ): 
-        print(velocidade)
+space,time_=input().split()
+space=int(space)
+time_=int(time_)
+velocidade =int(space/time_)
+print(velocidade)
